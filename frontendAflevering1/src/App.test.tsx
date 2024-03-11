4cb2aa7f3e938e93e4ad19095cef15723dcf78e4
Mads Hauberg <mads095h@gmail.com>
2024-03-11 22:55:03 +0900

    Removed unused import and formattet

--- frontendAflevering1/src/App.test.tsx
+++ frontendAflevering1/src/App.test.tsx
@@ -1,23 +1,23 @@
-import { render, screen, queryByAttribute  } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { describe, expect, it, } from "vitest";
-import '@testing-library/jest-dom'
-import App from "./App";
+import { render, queryByAttribute } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { describe, expect, it } from "vitest"
+import "@testing-library/jest-dom"
+import App from "./App"
 
 describe(App.name, () => {
-    it("should render", async () => {
-      const user = userEvent.setup();
+  it("should render", async () => {
+    const user = userEvent.setup()
 
-      const getById = queryByAttribute.bind(null, 'id');
+    const getById = queryByAttribute.bind(null, "id")
 
-      const dom = render(<App />);
-      const country = getById(dom.container, 'country');
-      const number = getById(dom.container, 'nummer');
+    const dom = render(<App />)
+    const country = getById(dom.container, "country")
+    const number = getById(dom.container, "nummer")
 
-      if (country && number) {
-        await user.selectOptions(country, "Danmark")
-        await user.type(number, "1234")
-      }
-      expect(number?.classList.contains("number_error")).toBe(true)
-    });
-  });
+    if (country && number) {
+      await user.selectOptions(country, "Danmark")
+      await user.type(number, "1234")
+    }
+    expect(number?.classList.contains("number_error")).toBe(true)
+  })
+})
