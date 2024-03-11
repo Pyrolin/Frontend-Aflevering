efadaf51aca31c75d9bfdd6e3d8d749ddd414414
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2024-03-11 15:03:52 +0100

    test

--- frontendAflevering1/src/App.test.tsx
+++ frontendAflevering1/src/App.test.tsx
@@ -1,23 +1,30 @@
-import { render, screen, queryByAttribute  } from "@testing-library/react";
+
+import { render, screen,  } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, expect, it, } from "vitest";
 import '@testing-library/jest-dom'
 import App from "./App";
 
-describe(App.name, () => {
+describe('App', () => {
+    it("displays error message for invalid email", async () => {
+        render(<App />);
+        
+        const emailInput = screen.getByLabelText("Email:");
+    
+    await userEvent.type(emailInput, "invalid-email");
+    
+    expect(emailInput).toHaveClass("number_error");
+      });
     it("should render", async () => {
-      const user = userEvent.setup();
+      render(<App />);
+      
+      const country = screen.getByLabelText("Country:");
+      const number = screen.getByLabelText("Telefon nummer:");
 
-      const getById = queryByAttribute.bind(null, 'id');
-
-      const dom = render(<App />);
-      const country = getById(dom.container, 'country');
-      const number = getById(dom.container, 'nummer');
-
-      if (country && number) {
-        await user.selectOptions(country, "Danmark")
-        await user.type(number, "1234")
-      }
-      expect(number?.classList.contains("number_error")).toBe(true)
+      await userEvent.selectOptions(country, "Danmark");
+      await userEvent.type(number, "1234");
+      
+      expect(number).toHaveClass("number_error");
     });
   });
+
